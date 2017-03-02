0ea93ea7d9f2a06452a0e97b83d73d2c961f11d4
Karadjordje <sveljod@gmail.com>
2017-03-02 01:27:56 +0100

    Nav is finished also made layout for rest of website.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById('mySidenav').style.width = '350px';
+}
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById('mySidenav').style.width = '0';
+}
