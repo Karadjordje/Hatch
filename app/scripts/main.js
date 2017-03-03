efa548542cc44f25602d8d51148ae6ff52fc3d88
Karadjordje <sveljod@gmail.com>
2017-03-03 18:58:17 +0100

    Footer finished and added some fixes for responsiveness.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById('mySidenav').style.width = '350px';
+    document.getElementById('mySidenav').style.width = '320px';
 }
 /* Set the width of the side navigation to 0 */
 function closeNav() {
