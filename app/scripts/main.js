59e1d6f69df261e2e53e60a53a172f5f4b4a3cc0
Karadjordje <sveljod@gmail.com>
2017-03-03 19:38:08 +0100

    Finished responsiveness.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,7 @@
 
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById('mySidenav').style.width = '320px';
+    document.getElementById('mySidenav').style.width = '300px';
 }
 /* Set the width of the side navigation to 0 */
 function closeNav() {
