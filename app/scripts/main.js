890c2db6a516c3af425c8f5b4b6e430544a6ba22
Karadjordje <sveljod@gmail.com>
2017-03-03 16:02:34 +0100

    Finished slider working on map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -30,3 +30,72 @@ $('.changeImageTwo').hover(function(){
 $('.changeImageThree').hover(function(){
 	$('#changeThisImage').attr('src','images/yellowhair.jpg');
 });
+
+$(document).ready(function() {
+    $('#lightSlider').lightSlider({
+        item: 6,
+        autoWidth: false,
+        slideMove: 6, // slidemove will be 1 if loop is true
+        slideMargin: 0,
+ 
+        addClass: '',
+        mode: 'slide',
+        useCSS: true,
+        cssEasing: 'ease', //'cubic-bezier(0.25, 0, 0.25, 1)',//
+        easing: 'linear', //'for jquery animation',////
+ 
+        speed: 400, //ms'
+        auto: true,
+        loop: true,
+        slideEndAnimation: false,
+        pause: 2000,
+ 
+        keyPress: false,
+        controls: true,
+        prevHtml: '<span class="custom-prev-html"><img src="images/leftArrow.png" /></span>',
+        nextHtml: '<span class="custom-next-html"><img src="images/rightArrow.png" /></span>',
+ 
+        rtl:false,
+        adaptiveHeight:false,
+ 
+        vertical:false,
+        verticalHeight:500,
+        vThumbWidth:100,
+ 
+        thumbItem:0,
+        pager: false,
+        gallery: false,
+        galleryMargin: 0,
+        thumbMargin: 0,
+        currentPagerPosition: 'middle',
+ 
+        enableTouch:true,
+        enableDrag:true,
+        freeMove:true,
+        swipeThreshold: 40,
+ 
+        responsive : [
+        	{
+        		breakpoint:800,
+                settings: {
+                    item:4,
+                    slideMove:1,
+                  }
+        	},
+        	{
+        		breakpoint:480,
+                settings: {
+                    item:2,
+                    slideMove:1,
+                  }
+        	}
+        ],
+ 
+        onBeforeStart: function (el) {},
+        onSliderLoad: function (el) {},
+        onBeforeSlide: function (el) {},
+        onAfterSlide: function (el) {},
+        onBeforeNextSlide: function (el) {},
+        onBeforePrevSlide: function (el) {}
+    });
+});
